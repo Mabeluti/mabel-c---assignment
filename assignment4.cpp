4474331c776916455c11202e14e1ca9a39f02492
mabeluti <maynkemuti@gmail.com>
2015-05-22 13:55:21 +0100

    my c++ assignment

---
+++ assignment4.cpp
@@ -0,0 +1,57 @@
+/*
+ *  Question4.cpp
+ *
+ *  Name:           [ mabel nkem]
+ *  Department:     [ mathematics]
+ *
+ *  Program that inputs a five-digit integer,
+ *  separates the integer into its digits and prints them separated by three spaces each
+ *
+ */
+ 
+ #include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "*********\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*       *\n"
+	     << "*********\n";
+	     cout << "   ** \n"
+	         << " *    * \n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << "*      *\n"
+	         << " *    *\n"
+	         << "   **  \n";
+	     cout << "  *   \n"
+	          << " ***  \n"
+	          << "***** \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n"
+	          << "  *   \n";
+	     cout << "    *   \n"
+	          << "   * *    \n"
+	         << " *     *  \n"
+	        << "*       *\n"
+	        << " *     * \n"
+	        << "   * *   \n"
+	        << "    *    \n";
+	        
+	        return 0;
+	        system("PAUSE");
+	        
+	        return 'EXIT_SUCCESS';
+}
